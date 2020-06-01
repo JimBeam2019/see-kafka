95adaa6998e8aa28cebe58ca6609f5ca10184dce
JimBeam2019 <bingking68@hotmail.com>
2020-06-01 12:29:42 +0800

    Early Setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # see-kafka
 See Kafka
+
+How to run
+1. If you have docker, after the docker image has been built simply run `docker-compose run --build -d` to build and run the project in docker.
+2. If you just wanna run it locally, another way is to run `npm run start`.
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+
+#include "rdkafkacpp.h"
+
+using namespace std;
+
+/**
+ * Main function.
+ */
+int main(int argc, char const *argv[]) {
+
+    cout << "Get started.. " << endl;
+    
+    return EXIT_SUCCESS;
+}
